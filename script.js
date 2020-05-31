2728dd4ab0fe8784c012c546ba82d1d8ed14f18e
Toufik Desai <60398527+Toufik353@users.noreply.github.com>
2020-05-31 21:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var formcloser = document.querySelector(".cross-icon-wrapper");
-var formmain = document.querySelector(".form");
-var formpageclicker = document.querySelector(".form-page-clicker");
-var quote = document.querySelector(".get-quote-item");
-var quote2 = document.querySelector(".get-quote-color");
+var form-closer = document.querySelector(".cross-icon-wrapper");
+var main-form = document.querySelector(".form");
+var clicker-form = document.querySelector(".form-page-clicker");
+var quoteitem = document.querySelector(".get-quote-item");
+var quotecolor = document.querySelector(".get-quote-color");
 var links = document.getElementsByTagName("a");
-var mainmenu = document.querySelector(".hamburger-button");
+var main-menu = document.querySelector(".hamburger-button");
 var lineone = document.getElementById("line1");
 var linetwo = document.getElementById("line2");
-var navbar = document.querySelector(".menu-bar");
+var menubar = document.querySelector(".menu-bar");
 var flag = 0;
-var overlap = document.querySelector(".overlay");
+var over-lay = document.querySelector(".overlay");
 var hide = document.querySelector(".overlay");
 
 var x = window.matchMedia("(max-width: 991px)");
@@ -17,34 +17,34 @@ myFunction(x);
 x.addListener(myFunction);
 function myFunction(x) {
   if (x.matches) {
-    navbar.style.height = "0";
-    navbar.style.opacity = "0";
+    .style.height = "0";
+    menubar.style.opacity = "0";
     lineone.style.transform = "translate(-50%, -4px)";
     linetwo.style.transform = "translate(-50%, 8px)";
-    navbar.style.transition = "none";
+    menubar.style.transition = "none";
 
-    mainmenu.onclick = function () {
+    main-menu.onclick = function () {
       if (flag == "0") {
         console.log(123);
         lineone.style.transform = "translate(-50%, 0px) rotate(45deg)";
         linetwo.style.transform = "translate(-50%, 0px) rotate(-45deg)";
-        navbar.style.transition = "all 0.2s linear";
-        navbar.style.height = "360px";
-        navbar.style.opacity = "1";
+        menubar.style.transition = "all 0.2s linear";
+        menubar.style.height = "360px";
+        menubar.style.opacity = "1";
 
         flag = 1;
       } else if (flag == 1) {
         lineone.style.transform = "translate(-50%, -4px)";
         linetwo.style.transform = "translate(-50%, 8px)";
-        navbar.style.opacity = "0";
-        navbar.style.height = "0";
+        menubar.style.opacity = "0";
+        menubar.style.height = "0";
 
         flag = 0;
       }
     };
   } else {
-    navbar.style.height = "auto";
-    navbar.style.opacity = "1";
+    menubar.style.height = "auto";
+    menubar.style.opacity = "1";
   }
 }
 
@@ -54,28 +54,28 @@ for (var i = 0; i < links.length; i++) {
   });
 }
 
-formcloser.onclick = function () {
-  formmain.style.transform = "translateX(100%)";
-  overlap.style.display = "none";
-  overlap.style.opacity = "0";
+form-closer.onclick = function () {
+  main-form.style.transform = "translateX(100%)";
+  over-lay.style.display = "none";
+  over-lay.style.opacity = "0";
 };
-formpageclicker.onclick = function () {
-  formmain.style.transform = "translateX(0)";
-  overlap.style.display = "block";
-  overlap.style.opacity = "1";
+clicker-form.onclick = function () {
+  main-form.style.transform = "translateX(0)";
+  over-lay.style.display = "block";
+  over-lay.style.opacity = "1";
 };
-quote.onclick = function () {
-  formmain.style.transform = "translateX(0)";
-  overlap.style.display = "block";
-  overlap.style.opacity = "1";
+quoteitem.onclick = function () {
+  main-form.style.transform = "translateX(0)";
+  over-lay.style.display = "block";
+  over-lay.style.opacity = "1";
 };
-quote2.onclick = function () {
-  formmain.style.transform = "translateX(0)";
-  overlap.style.display = "block";
-  overlap.style.opacity = "1";
+quotecolor.onclick = function () {
+  main-form.style.transform = "translateX(0)";
+  over-lay.style.display = "block";
+  over-lay.style.opacity = "1";
 };
 hide.onclick = function () {
-  overlap.style.display = "none";
-  overlap.style.opacity = "0";
-  formmain.style.transform = "translateX(100%)";
+    over-lay.style.display = "none";
+    over-lay.style.opacity = "0";
+  main-form.style.transform = "translateX(100%)";
 };
